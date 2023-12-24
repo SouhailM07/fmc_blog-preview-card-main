d4f4bb4809a1f7f610638910d5d7fa0e04acd0f8
shadow <lightshadow416@gmail.com>
2023-12-24 13:46:21 +0100

    project changes : 1-> adding fonts , |Figlet /600 / 800| 2-> adding colors to tailwind config file 3-> [MyContainer] component was created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 /** @type {import(tailwindcss).Config} */ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        Yellow: "#FFD700",
+        White: "#FFFFFF",
+        Grey: "#808080",
+        Black: "#1C1C1C",
+      },
+    },
+  },
   plugins: [],
 };
